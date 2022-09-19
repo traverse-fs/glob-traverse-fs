ce12007d8b5243cc4a1d55edeaae8ab718c58901
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-09-19 09:22:28 -0700

    [COMMIT] First commit of traverse-cli, traverse-fs, fssys

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * Package: 
  * Author: Ganesh B
  * Description: Nodejs npm module to traverse folder using code or cli or use glob patterns
- * Install: npm i 
+ * Install: npm i traverse-cli, npm i traverse-fs, npm i fssys
  * Github: https://github.com/ganeshkbhat/glob-traverse-fs
  * npmjs Link: 
  * File: index.js
