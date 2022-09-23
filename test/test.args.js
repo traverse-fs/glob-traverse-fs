842c54e2856f9e8a9f95cad9abd22739482eca36
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-09-22 20:08:44 -0700

    [COMMIT] Minor changes to traverse-fs files. Allowing function to keep the util function to get commandline process arguments

---
+++ test/test.args.js
@@ -0,0 +1,97 @@
+// /**
+//  * 
+//  * Package: 
+//  * Author: Ganesh B
+//  * Description: Nodejs npm module to traverse folder using code or cli or use glob patterns
+//  * Install: npm i 
+//  * Github: https://github.com/ganeshkbhat/glob-traverse-fs
+//  * npmjs Link: 
+//  * File: test.args.js
+//  * Test for File: traverse.js
+//  * File Description: Traverse folder and files core file
+//  * 
+// */
+
+
+// var expect = require('chai').expect;
+
+// describe('test.args.js::traverse:fssys:traverse-cli: Test Suite for Traverse cli args', function() {
+
+//     describe ('test.args.js::traverse:fssys:traverse-cli: [Test A] Test Suite for ', function() {
+//         // it('[Test A] package.json Present', function(done){
+//         //     expect(200).to.equal(200);
+//         //     done();
+//         // });
+
+//         it('[Test A] ', function(done) {
+//             expect(100).to.equal(100);
+//             done();
+//         });
+
+//     });
+
+//     describe ('test.args.js::traverse:fssys:traverse-cli: [Test B] Test Suite for ', function() {
+//         // it('[Test B] status', function(done){
+//         //     expect(200).to.equal(200);
+//         //     done();
+//         // });
+
+//         it('[Test B] ', function(done) {
+//             expect(100).to.equal(100);
+//             done();
+//         });
+
+//     });
+
+//     describe ('test.args.js::traverse:fssys:traverse-cli: [Test C] Test Suite Recursive for ', function() {
+//         // it('[Test C] status', function(done){
+//         //     expect(200).to.equal(200);
+//         //     done();
+//         // });
+
+//         it('[Test C] ', function(done) {
+//             expect(100).to.equal(100);
+//             done();
+//         });
+
+//     });
+
+//     describe ('test.args.js::traverse:fssys:traverse-cli: [Test D] Test Suite Recursive for ', function() {
+//         // it('[Test D] status', function(done){
+//         //     expect(200).to.equal(200);
+//         //     done();
+//         // });
+
+//         it('[Test D] ', function(done) {
+//             expect(100).to.equal(100);
+//             done();
+//         });
+
+//     });
+
+//     describe ('test.args.js::traverse:fssys:traverse-cli: [Test E] Test Suite Negatives ', function() {
+//         // it('[Test E] status', function(done){
+//         //     expect(200).to.equal(200);
+//         //     done();
+//         // });
+
+//         it('[Test E] ', function(done) {
+//             expect(100).to.equal(100);
+//             done();
+//         });
+
+//     });
+
+//     describe ('test.args.js::traverse:fssys:traverse-cli: [Test F] Test Suite Negatives for ', function() {
+//         // it('[Test F] status', function(done){
+//         //     expect(200).to.equal(200);
+//         //     done();
+//         // });
+
+//         it('[Test F] ', function(done) {
+//             expect(100).to.equal(100);
+//             done();
+//         });
+
+//     });
+// });
